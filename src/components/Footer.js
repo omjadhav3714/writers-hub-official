5942c89183f700a4297f368f16e0c1540e03246e
AniketS01 <aniketshetyeajs@gmail.com>
2021-09-01 13:03:08 +0530

    footer added and font fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
           <div class='col-lg-6 col-md-12 mb-4 mb-md-0'>
             <h4 class='text-uppercase'>About us</h4>
 
-            <p className='s-3'>
+            <p className='s-3' style={{ fontSize: '22px' }}>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
               atque ea quis molestias. Fugiat pariatur maxime quis culpa
               corporis vitae repudiandae aliquam voluptatem veniam, est atque
@@ -28,7 +28,10 @@ const Footer = () => {
             <ul class='list-unstyled mb-0'></ul>
           </div>
 
-          <div class='col-lg-3 col-md-6 mb-4 mb-md-0'>
+          <div
+            class='col-lg-3 col-md-6 mb-4 mb-md-0'
+            style={{ fontSize: '22px' }}
+          >
             <ul class='list-unstyled'>
               <li>
                 <Link
@@ -71,7 +74,7 @@ const Footer = () => {
         </div>
       </div>
       <hr />
-      <div class='text-center p-3'>
+      <div class='text-center p-3' style={{ fontSize: '22px' }}>
         © 2020 Copyright:
         <Link class='text-white' href='https://mdbootstrap.com/'>
           Writers Hub official
