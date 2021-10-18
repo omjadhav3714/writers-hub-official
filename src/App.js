566ed15d798efda5cfa25341c25bd2c722e97926
AniketS01 <aniketshetyeajs@gmail.com>
2021-10-18 14:30:04 +0530

    protected route

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Kavita from './pages/Kavita';
 import Blog from './pages/Blog';
 import SignUp from './components/forms/SignUp';
 import LogIn from './components/forms/LogIn';
-import { AuthProvider } from './context/AuthContext';
+//import { AuthProvider } from './context/AuthContext';
 import ForgetPassword from './components/forms/ForgetPassword';
 import UserProfile from './pages/UserProfile';
 import Approvals from './pages/Approvals';
@@ -23,33 +23,62 @@ import KavitaApprovals from './pages/approvals/KavitaApprovals';
 import QuoteApprovals from './pages/approvals/quoteApproval';
 import ShayriApprovals from './pages/approvals/ShayriApproval';
 import BlogApprovals from './pages/approvals/BlogApprovals';
+import ProtectedRoute from './components/ProtectedRoute';
+import { useAuth } from './context/AuthContext';
+import { Flag } from 'react-bootstrap-icons';
 
 const App = () => {
+  const { currentUser } = useAuth();
+  {
+    console.log(currentUser);
+  }
   return (
-    <AuthProvider>
-      <AnimatePresence>
-        <Switch>
-          <Route path='/approvals/blogs/:id' component={BlogApprovals} />
-          <Route path='/approvals/shayris/:id' component={ShayriApprovals} />
-          <Route path='/approvals/quotes/:id' component={QuoteApprovals} />
-          <Route path='/approvals/kavitas/:id' component={KavitaApprovals} />
-          <Route path='/approvals' component={Approvals} />
-          <Route path='/users/:id' component={UserProfile} />
-          <Route path='/blogs/:id' component={Blog} />
-          <Route path='/kavitas/:id' component={Kavita} />
-          <Route path='/quotes/:id' component={Quotes} />
-          <Route path='/shayaris/:id' component={Shayri} />
-          <Route path='/quotes' component={QuotePage} />
-          <Route path='/kavitas' component={KavitaPage} />
-          <Route path='/shayaris' component={ShayariPage} />
-          <Route path='/blogs' component={BlogPage} />
-          <Route path='/resetpassword' component={ForgetPassword} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/login' component={LogIn} />
-          <Route exact path='/' component={HomePage} />
-        </Switch>
-      </AnimatePresence>
-    </AuthProvider>
+    <AnimatePresence>
+      <Switch>
+        <Route path="/blogs/:id" component={Blog} />
+        <Route path="/kavitas/:id" component={Kavita} />
+        <Route path="/quotes/:id" component={Quotes} />
+        <Route path="/shayaris/:id" component={Shayri} />
+        <Route path="/quotes" component={QuotePage} />
+        <Route path="/kavitas" component={KavitaPage} />
+        <Route path="/shayaris" component={ShayariPage} />
+        <Route path="/blogs" component={BlogPage} />
+        <Route path="/resetpassword" component={ForgetPassword} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={LogIn} />
+        <Route exact path="/" component={HomePage} />
+        <ProtectedRoute
+          path="/approvals"
+          component={Approvals}
+          auth={currentUser ? true : false}
+        />
+        <ProtectedRoute
+          path="/approvals/blogs/:id"
+          component={BlogApprovals}
+          auth={currentUser ? true : false}
+        />
+        <ProtectedRoute
+          path="/approvals/shayris/:id"
+          component={ShayriApprovals}
+          auth={currentUser ? true : false}
+        />
+        <ProtectedRoute
+          path="/approvals/quotes/:id"
+          component={QuoteApprovals}
+          auth={currentUser ? true : false}
+        />
+        <ProtectedRoute
+          path="/approvals/kavitas/:id"
+          component={KavitaApprovals}
+          auth={currentUser ? true : false}
+        />
+        <ProtectedRoute
+          path="/users/:id"
+          component={UserProfile}
+          auth={currentUser ? true : false}
+        />
+      </Switch>
+    </AnimatePresence>
   );
 };
 
